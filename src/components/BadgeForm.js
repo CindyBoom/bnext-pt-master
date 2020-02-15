f2bb2570e1701acb0f5e2090d813c2d8ee822a79
CindyBoom <cinl.dct@gmail.com>
2020-02-15 14:58:20 -0600

    este es el bueno

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 class BadgeForm extends React.Component {
-  state = {};
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      loanamount: 0,
+    };
+    this.handleChange =
+    this.handleChange.bind(this);
+  }
   async componentDidMount() {
     const { id } = this.props;
 
@@ -11,11 +17,11 @@ class BadgeForm extends React.Component {
 
     const response = await fetch(`https://api.bnext.io/partner_test/user?id=${ id }`, {
       method: 'GET',
-      headers
+      headers: headers
     });
 
     if (response.status === 404) {
-      this.setState({ notFound: true });
+      this.setState({ id: "Usuario no encontrado" });
     } else {
       const { data } = await response.json();
       this.setState(data);
@@ -26,28 +32,48 @@ class BadgeForm extends React.Component {
     this.setState({
       [e.target.name]: e.target.value,
     });
+    if([e.target.name].value!=='') {
+      console.log('no es un buen valor para este input');
+    }
   }
 
   handleClick(e) {
-    console.log('Button was clicked');
-  }
-
-  handleSubmit(e) {
     e.preventDefault();
     console.log('Button was clicked');
+    const dataToSend = {
+      id: document.querySelector('[name="id"]').value,
+      name: document.querySelector('[name="name"]').value,
+      surname: document.querySelector('[name="surname"]').value,
+      email: document.querySelector('[name="email"]').value,
+      phone: document.querySelector('[name="phone"]').value,
+      age: document.querySelector('[name="age"]').value,
+      loan_amount: document.querySelector('[name="loanamount"]').value,
+      loan_date: document.querySelector('[name="loandate"]').value,
+      loan_weeks: document.querySelector('[name="loanweeks"]').value,
+    };
+    const headers = new Headers();
+    headers.set('X-WEB-KEY', 'Development');
+    headers.set('Content-Type', 'application/json');
+    fetch(`https://api.bnext.io/partner_test/user/${dataToSend.id}`, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(dataToSend)
+    })
+      .then(response => console.log(response) )
   }
 
+
+
   render() {
-    return this.state.notFound ?
-      (
-        <div className="not-found d-flex justify-content-center align-items-center">
-          <h1>Not Found</h1>
-        </div>
-      ) :
-      (
+    const formatter = new Intl.NumberFormat('es-MX', {
+      style: 'currency',
+      currency: 'MXN'
+    });
+
+    return (
         <div>
           <h1>Registro Nuevo</h1>
-          <form onSubmit={this.handleSubmit}>
+          <form >
             <div className="form-group">
               <label>ID</label>
               <input
@@ -60,24 +86,28 @@ class BadgeForm extends React.Component {
             </div>
 
             <div className="form-group">
-              <label>First Name</label>
+              <label>Name</label>
               <input
                 onChange={this.handleChange}
                 className="form-control"
                 type="text"
                 name="name"
+                id="name"
                 value={this.state.name}
+                type="text" name="name" readonly
               />
+            
             </div>
 
             <div className="form-group">
-              <label>Last Name</label>
+              <label>Surname</label>
               <input
                 onChange={this.handleChange}
                 className="form-control"
                 type="text"
                 name="surname"
                 value={this.state.surname}
+                type="text" name="surname" readonly
               />
             </div>
 
@@ -89,6 +119,7 @@ class BadgeForm extends React.Component {
                 type="text"
                 name="email"
                 value={this.state.email}
+                type="text" name="email" readonly
               />
             </div>
             <div className="form-group">
@@ -112,6 +143,55 @@ class BadgeForm extends React.Component {
                 value={this.state.age}
               />
             </div>
+
+            <div className="form-group">
+              <label>Loan amount</label>
+              <input
+                onChange={this.handleChange}
+                className="form-control"
+                type="number"
+                min="10"
+                max="1000"
+                name="loanamount"
+                value={this.state.loanamount}
+              />
+              <br />
+              {formatter.format(this.state.loanamount)}
+            </div>
+            <div className="form-group">
+              <label>Loan date</label>
+              <input
+                onChange={this.handleChange}
+                className="form-control"
+                type="date"
+                name="loandate"
+                value={this.state.loandate}
+              />
+            </div>
+            <div className="form-group">
+              <label>Loan weeks</label>
+              <select
+                onChange={this.handleChange}
+                className="form-control"
+                type="text"
+                name="loanweeks"
+                value={this.state.loanweeks}>
+                <option>Select an option</option>
+                {Array.from('x'.repeat(20)).map((_, index) => <option value={index+1}>
+                  {index+1} semana/s
+                </option>)}
+              </select>
+            </div>
+            <div className="form-group">
+              <label><a href="https://comparte.bnext.io/privacidad">Check</a></label>
+              <input
+                onChange={this.handleChange}
+                className="form-control"
+                type="checkbox"
+                name="check"
+                checked={this.state.check ? true : false}
+              />
+                </div>
 
             <button onClick={this.handleClick} className="btn btn-primary">
               Save
